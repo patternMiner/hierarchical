097bc54d9b1b7c28c168f6d086989c7d6342a7c5
Jagdish Bisa <jbisa61@gmail.com>
2014-03-13 07:02:20 -0700

    Added isOnScrollbar method.

--- lib/component/tree_chip/tree_chip.dart
+++ lib/component/tree_chip/tree_chip.dart
@@ -18,19 +18,24 @@ class TreeChipComponent {
   Iterable items;
 
   void onMouseDown(MouseEvent event) {
+    if (isOnScrollbar(event)) {
+      print("clicked on the scrollbar");
+    }
+  }
+
+  bool isOnScrollbar(MouseEvent event) {
     Element element = event.target as Element;
     bool isOverflow = element.scrollWidth > element.clientWidth ||
         element.scrollHeight > element.clientHeight;
     if (isOverflow) {
       int scrollbarWidth = element.offsetWidth - element.clientWidth;
+      int scrollbarHeight = element.offsetHeight - element.clientHeight;
       Rectangle bounds = element.getBoundingClientRect();
-
-      double maxX = bounds.left + bounds.width;
-      double minX = maxX - scrollbarWidth;
-      int clientX = event.clientX;
-      if (clientX >= minX && clientX <= maxX) {
-        print("clicked on the scrollbar");
-      }
+      Rectangle realBounds =
+          new Rectangle(bounds.left, bounds.top,
+              bounds.width - scrollbarWidth, bounds.height - scrollbarHeight);
+      return !realBounds.containsPoint(event.client);
     }
+    return false;
   }
 }
